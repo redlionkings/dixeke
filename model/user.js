12348e14df03d3a85d49bfe910c1ebf768fe2678
hungdt1993 <duongthanhhung1993@gmail.com>
2019-06-11 20:24:12 +0700

    commit 3

---
+++ model/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema({
+    email : {type : String, required: true, unique : true},
+    password : {type: String, required : true},
+    fullName : {type :String, required : true},
+    userType : {type: String, required: true},
+    phone : {type: Number, required : true, unique:true},
+    DOB:{type :Date ,required: true},
+    registerDate : {
+        type :Date ,
+        default: new Date().getTime()
+    },
+    numberOfTrips :{type: Number},
+    numberOfKms: {type: Number},
+    avatar: {type : String},
+    isActive:{type: Boolean, default: true}
+})
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = {
+    User, UserSchema
+}
